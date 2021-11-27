cc96c76736554a8c477b8ccc1b15c8e5dfbef103
Muhammed Ali Nayir <malinayir@gmail.com>
2021-11-27 20:32:43 +0300

    Heroku file added

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors())
 
-app.use('/posts', postRoutes);
 
+app.use('/posts', postRoutes);
+app.get('/', (req, res) => {
+    res.send('Api Working...');
+})
 
 const PORT = process.env.PORT;
 
